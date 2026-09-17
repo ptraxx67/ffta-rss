34fed7f873e5bfc353b7ebf559d568f9986b454a
ptraxx67 <ptraxx67@gmail.com>
2026-09-17 16:46:03 +0200

    Update generate-rss.py

--- generate-rss.py
+++ generate-rss.py
@@ -28,7 +28,7 @@ end_date = date.today()
 # On utilise 365 jours pour éviter les problèmes avec le 29 février.
 start_date = end_date - timedelta(days=365)
 
-FFTA_RESULT_URL = (
+FFTA_URL = (
     "https://www.ffta.fr/competitions"
     "?search="
     f"&start={start_date.isoformat()}"
@@ -41,8 +41,8 @@ FFTA_RESULT_URL = (
     "&sort_order=DESC"
 )
 
-print("URL Résultats FFTA utilisée :")
-print(FFTA_RESULT_URL)
+print("URL FFTA utilisée :")
+print(FFTA_URL)
 print()
 
 
@@ -61,7 +61,7 @@ BROWSERLESS_URL = (
 # ============================================================
 
 payload = {
-    "url": FFTA_RESULT_URL,
+    "url": FFTA_URL,
 
     "elements": [
         {
@@ -230,7 +230,7 @@ SubElement(
     "description"
 ).text = (
     "Résultats des compétitions FFTA "
-    "pour le département 57."
+    "pour le département 58."
 )
 
 SubElement(
@@ -284,7 +284,7 @@ for competition in competitions:
 tree = ElementTree(rss)
 
 tree.write(
-    "FFTA_Resultats.xml",
+    "ffta.xml",
     encoding="utf-8",
     xml_declaration=True
 )
@@ -295,7 +295,7 @@ print("========================================")
 print("RSS généré avec succès !")
 print("========================================")
 print()
-print("Fichier créé : FFTA_Resultats.xml")
+print("Fichier créé : ffta.xml")
 print()
 print(
     f"Période FFTA : {start_date.isoformat()} "
